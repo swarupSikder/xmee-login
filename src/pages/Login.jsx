d3519b1fe30a589eeacf7e2228f361be127420e8
swarupSikder <swarupfx20@gmail.com>
2025-07-06 23:31:06 +0600

    login page design initialized

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,24 +7,29 @@ const Login = () => {
             <div className='container mx-auto p-24 flex items-center justify-between'>
                 {/* Left Part */}
                 <div>
-                    <h1 className='text-purple-700 text-6xl font-extrabold'>Xmee</h1>
+                    <h1 className='text-sky-700 text-6xl font-extrabold font-audiowide'>xmee</h1>
 
-                    <p>
+                    <p className='my-4 font-semibold text-2xl text-slate-400'>
                         Hurry! Signin to <br />
                         Explore Our Trending Services
                     </p>
 
-                    <p>
+                    <p className='font-semibold text-sky-700 mt-16'>
                         If you don't have an account <br />
                         Please sign up
                     </p>
                 </div>
 
                 {/* Right Part */}
-                <div>
-                    <input type="email" name="email" placeholder='example@email.com' />
-                    <input type="password" name="password" placeholder='******' />
-                    <input type="submit" value="Login" className='btn btn-primary' />
+                <div className="card-body">
+                    <fieldset className="fieldset">
+                        <label className="label">Email</label>
+                        <input type="email" className="input" placeholder="Email" />
+                        <label className="label">Password</label>
+                        <input type="password" className="input" placeholder="Password" />
+                        <div><a className="link link-hover">Forgot password?</a></div>
+                        <button className="btn btn-neutral mt-4 w-fit">Login</button>
+                    </fieldset>
                 </div>
             </div>
         </div>
