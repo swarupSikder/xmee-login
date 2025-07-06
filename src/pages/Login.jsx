128d0783da1238dddbc60bf63d1054221a11e28c
swarupSikder <swarupfx20@gmail.com>
2025-07-06 23:20:47 +0600

    google font added and login page modified

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,12 +3,29 @@ import React from 'react';
 const Login = () => {
     return (
         <div className='min-h-screen relative'>
-            <div>
 
-            </div>
+            <div className='container mx-auto p-24 flex items-center justify-between'>
+                {/* Left Part */}
+                <div>
+                    <h1 className='text-purple-700 text-6xl font-extrabold'>Xmee</h1>
 
-            <div>
+                    <p>
+                        Hurry! Signin to <br />
+                        Explore Our Trending Services
+                    </p>
 
+                    <p>
+                        If you don't have an account <br />
+                        Please sign up
+                    </p>
+                </div>
+
+                {/* Right Part */}
+                <div>
+                    <input type="email" name="email" placeholder='example@email.com' />
+                    <input type="password" name="password" placeholder='******' />
+                    <input type="submit" value="Login" className='btn btn-primary' />
+                </div>
             </div>
         </div>
     );
